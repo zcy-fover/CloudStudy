d34b49d43c424068e5a5d8923a6f878ca2b27d76
zcy-fover <15207189058@163.com>
2019-03-24 17:11:16 +0800

    eureka集群搭建示例代码

---
+++ eureka-server/src/main/java/com/zcyfover/spring/config/WebSecurityConfigurer.java
@@ -0,0 +1,18 @@
+package com.zcyfover.spring.config;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ */
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
